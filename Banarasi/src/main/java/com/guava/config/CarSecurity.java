82be87a51f7ba937be2b8666fbabb62a1117e4ea
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-05 20:21:44 +0530

    return http.build();

---
+++ Banarasi/src/main/java/com/guava/config/CarSecurity.java
@@ -0,0 +1,31 @@
+package com.guava.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class CarSecurity
+{
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception
+    {
+//        ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)http.authorizeHttpRequests().anyRequest()).authenticated();
+//        http.formLogin();
+//        http.httpBasic();
+//        return (SecurityFilterChain)http.build();
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/car/test").permitAll()
+                .requestMatchers("/car/").authenticated().
+                and().formLogin()
+                .and().httpBasic();
+
+        return http.build();
+
+
+    }
+}
