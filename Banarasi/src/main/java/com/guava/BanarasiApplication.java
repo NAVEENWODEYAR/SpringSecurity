acf93e0c1410373cf775faa19d52037a9558853e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-05 19:27:56 +0530

    security

---
+++ Banarasi/src/main/java/com/guava/BanarasiApplication.java
@@ -0,0 +1,15 @@
+package com.guava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanarasiApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(BanarasiApplication.class, args);
+		System.out.println("************");
+	}
+
+}
---
+++ Banarasi/src/test/java/com/guava/BanarasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanarasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
