84ab508dc6f8979789525ffdb3c2fbf591bf89a9
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-06 13:25:27 +0530

    ResponseDTO added

---
+++ Banarasi/src/main/java/com/guava/dto/ResponseDTO.java
@@ -0,0 +1,14 @@
+package com.guava.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDTO
+{
+    private String msg;
+    private Object obj;
+}
