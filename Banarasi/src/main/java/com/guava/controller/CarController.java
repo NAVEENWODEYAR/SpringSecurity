75fae34529173ef19243a6f5016da8123c89921f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-06 13:33:01 +0530

    ResponseEntity<>(responseDTO,HttpStatus.FOUND);

--- Banarasi/src/main/java/com/guava/controller/CarController.java
+++ Banarasi/src/main/java/com/guava/controller/CarController.java
@@ -1,9 +1,12 @@
 package com.guava.controller;
 
 import com.guava.dto.CarRequest;
+import com.guava.dto.ResponseDTO;
 import com.guava.modal.Car;
 import com.guava.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,24 +19,28 @@ public class CarController
     @Autowired
     private CarService carService;
 
-    // API's
+    // API
     @GetMapping("/test")
     public String getMsg()
     {
         return carService.getMsg();
     }
 
+    // Insert into the table,
     @PostMapping
-    public Car insertCar(@RequestBody CarRequest carRequest)
+    public ResponseEntity<ResponseDTO> insertCar(@RequestBody CarRequest carRequest)
     {
-        Car car = new Car(carRequest);
-            return carService.insertCar(carRequest);
+        Car car = carService.insertCar(carRequest);
+        ResponseDTO responseDTO = new ResponseDTO("Car details saved successfully into the database,",car);
+            return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
     }
 
+    // Select * from table,
     @GetMapping
-    public List<Car> getCars()
+    public ResponseEntity<ResponseDTO>getCars()
     {
         List<Car> cars = carService.getCars();
-        return cars;
+        ResponseDTO responseDTO = new ResponseDTO("Available cars in the database are",cars);
+        return new ResponseEntity<>(responseDTO,HttpStatus.FOUND);
     }
 }
